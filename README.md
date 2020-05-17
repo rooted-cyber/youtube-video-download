3ab45609233680cf74105b11d7255b0d2b0b7e70
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2020-05-17 18:17:09 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# youtube-video-download
+<pre>
+ $ apt update
+ $ apt upgrade
+ $ apt install git
+ $ git clone https://github.com/rooted-cyber/youtube-video-download
+ $ cd youtube-video-download
+ $ pip install -r requirement.txt
+ $ bash download
+ </pre>
---
+++ download.sh
@@ -0,0 +1,43 @@
+termux-setup-storage
+R () {
+	cd $PREFIX/bin
+	if [ -e python ];then
+	#cd ~/Termux-Hack-2
+	cd /sdcard/Gith*
+	echo -e "\033[0m"
+	python .random.py
+	fi
+	}
+	set () {
+		R
+		printf "\n\n Installing..\n"
+		apt update
+		apt upgrade
+		apt install python
+		apt install figlet
+		apt install wget
+		cd ~/youtube-video-download
+		pip install -r requirement.txt
+		cd /sdcard
+		mkdir youtube-video-download
+		}
+		menu () {
+			R
+			figlet Youtube
+			echo
+			R
+			echo -e -n "Enter youtube video link: "
+			read a
+			if [ ! -z $a ];then
+			cd /sdcard/youtube-download
+			youtube-dl $a
+			R
+			printf "\nSaved ( /sdcard/youtube-video-download )"
+			fi
+			}
+			cd /sdcard
+			if [ -e youtube-video-download ];then
+			menu
+			else
+			set
+			fi
